d9a59e6e7ce8fecd20ec32baa2c1dc1007a912de
Jeserlin <jeserlin@me.com>
2019-05-26 17:37:30 +0800

    Add code pen examples.

--- my-tech-blog/src/containers/Bootstrap4Cards/Bootstrap4Cards.js
+++ my-tech-blog/src/containers/Bootstrap4Cards/Bootstrap4Cards.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 import { ArticleTitle, ArticleContent } from '../../components/Common/Common';
 
-const Card = styled.span`
-  margin-right: 1rem;
+const Iframe = styled.iframe`
+  width: 100%;
 `;
 
 class Bootstrap4Cards extends Component {
@@ -17,15 +17,24 @@ class Bootstrap4Cards extends Component {
           Card columns 可以將cards瀑布版的排列，<br />
           順序是 <span className='hightlight'>從上到下，再從左到右</span><br />
           也就是說假設我們寫了9個cards，最後呈現的順序會如下：<br />
-          <div className='emphasize'>
-            <Card>card1</Card><Card>card4</Card><Card>card7</Card><br />
-            <Card>card2</Card><Card>card5</Card><Card>card8</Card><br />
-            <Card>card3</Card><Card>card6</Card><Card>card9</Card><br />
-          </div>
+          <Iframe height="350" scrolling="no" title="Bootstrap 4.0 Cards" src="//codepen.io/jeserlin/embed/XwqYXr/?height=265&theme-id=0&default-tab=result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+            See the Pen
+            <a href='https://codepen.io/jeserlin/pen/XwqYXr/'>Bootstrap 4.0 Cards</a>
+            by jeserlin chiu
+            (<a href='https://codepen.io/jeserlin'>@jeserlin</a>) on <a href='https://codepen.io'>CodePen</a>.
+          </Iframe><br />
           目前似乎沒辦法透過css調整他的排序(? 如果有找到再來更新)<br />
-          官網中有提到另寫css調整不同大小下，每行顯示的cards數量：<br />
+          官網中有提到另寫scss調整不同解析度下，每行顯示的cards數量：<br />
+          <Iframe height="350" scrolling="no" title="Bootstrap 4 - Cards  Columns break point (css)" src="//codepen.io/jeserlin/embed/vwjrvR/?height=265&theme-id=0&default-tab=css" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+            See the Pen <a href='https://codepen.io/jeserlin/pen/vwjrvR/'>Bootstrap 4 - Cards  Columns break point (css)</a> by jeserlin chiu
+            (<a href='https://codepen.io/jeserlin'>@jeserlin</a>) on <a href='https://codepen.io'>CodePen</a>.
+          </Iframe><br/>
           其中的column-count就是在設定每行要顯示的cards數量。<br />
-          以下提供CSS的寫法 :<br />
+          以下提供css的寫法 :<br />
+          <Iframe height="350" scrolling="no" title="Boostrap 4 - Cards Columns break point (css)" src="//codepen.io/jeserlin/embed/pmVZJj/?height=329&theme-id=0&default-tab=css,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+            See the Pen <a href='https://codepen.io/jeserlin/pen/pmVZJj/'>Boostrap 4 - Cards Columns break point (css)</a> by jeserlin chiu
+            (<a href='https://codepen.io/jeserlin'>@jeserlin</a>) on <a href='https://codepen.io'>CodePen</a>.
+          </Iframe><br />
         </ArticleContent>
       </div>
     );
