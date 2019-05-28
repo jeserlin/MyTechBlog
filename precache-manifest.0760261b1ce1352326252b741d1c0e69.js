bfe8e93fce1b3107a5920d58bafa565e0765d3ee
Jeserlin <jeserlin@me.com>
2019-05-29 00:12:19 +0800

    Updates

---
+++ precache-manifest.0760261b1ce1352326252b741d1c0e69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac4eed79fe99c5e263b3",
+    "url": "/MyTechBlog/static/css/main.4fe1f130.chunk.css"
+  },
+  {
+    "revision": "ac4eed79fe99c5e263b3",
+    "url": "/MyTechBlog/static/js/main.b5e33bfb.chunk.js"
+  },
+  {
+    "revision": "9d4ebaa74210618b0d88",
+    "url": "/MyTechBlog/static/js/runtime~main.6302f219.js"
+  },
+  {
+    "revision": "58f903c09074d9f61ea8",
+    "url": "/MyTechBlog/static/js/2.f4926967.chunk.js"
+  },
+  {
+    "revision": "7c54f720790191a742efda6f194fc25a",
+    "url": "/MyTechBlog/index.html"
+  }
+];
