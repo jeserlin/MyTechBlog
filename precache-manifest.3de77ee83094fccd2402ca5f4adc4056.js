352f3d9990e415f2dc2ca0a2d5aec243b6a6a530
Jeserlin <jeserlin@me.com>
2019-08-04 19:36:30 +0800

    Updates

--- precache-manifest.3de77ee83094fccd2402ca5f4adc4056.js
+++ precache-manifest.3de77ee83094fccd2402ca5f4adc4056.js
@@ -1,23 +1,35 @@
 self.__precacheManifest = [
   {
-    "revision": "6d673a39a2eb22424845",
+    "revision": "137a3eeb5bf3e87d010f",
     "url": "/MyTechBlog/static/css/main.dd7bbe15.chunk.css"
   },
   {
-    "revision": "6d673a39a2eb22424845",
-    "url": "/MyTechBlog/static/js/main.1ff74305.chunk.js"
+    "revision": "137a3eeb5bf3e87d010f",
+    "url": "/MyTechBlog/static/js/main.14620b88.chunk.js"
   },
   {
     "revision": "9d4ebaa74210618b0d88",
     "url": "/MyTechBlog/static/js/runtime~main.6302f219.js"
   },
   {
-    "revision": "7ef721b2d39091ed0b8a",
-    "url": "/MyTechBlog/static/js/2.8b152f7d.chunk.js"
+    "revision": "e2fd9ccbadce52180ad3",
+    "url": "/MyTechBlog/static/js/2.62d23362.chunk.js"
+  },
+  {
+    "revision": "c03d5f8f03da46303a31387a077319b9",
+    "url": "/MyTechBlog/static/media/ga1_1.c03d5f8f.png"
+  },
+  {
+    "revision": "9f4cea27bb130bf91c535f3d9d0f0c6a",
+    "url": "/MyTechBlog/static/media/ga1_2.9f4cea27.png"
   },
   {
     "revision": "5d1cbe34a9735953fd1af6ac18c13f8c",
     "url": "/MyTechBlog/static/media/bootstrap4.5d1cbe34.png"
+  },
+  {
+    "revision": "af27fcd29fe3e3d137e4fd893c2e5da5",
+    "url": "/MyTechBlog/static/media/googleAnalytics.af27fcd2.png"
   },
   {
     "revision": "0291b1822ada87a90d327ebc99ce7241",
@@ -32,7 +44,7 @@ self.__precacheManifest = [
     "url": "/MyTechBlog/static/media/yuan.3474e5f3.png"
   },
   {
-    "revision": "53e1a59ab3835f4e5c82847dda298de1",
+    "revision": "5b75132dd75aa59a9f828008ee29abde",
     "url": "/MyTechBlog/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyTechBlog/precache-manifest.1c976b1ef54bd70f87095004f833d1ad.js"
+  "/MyTechBlog/precache-manifest.3de77ee83094fccd2402ca5f4adc4056.js"
 );
 
 workbox.clientsClaim();
